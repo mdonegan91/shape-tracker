50368b61e7e1b3d61452e176abe6167db5aa2c1d
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-30 21:09:28 -0800

    pass not a triangle test

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -2,10 +2,15 @@ import Triangle from './../src/triangle.js';
 
 describe('Triangle', () => {
 
-        test('should correctly creat a triangle obect with three lengths', () => {
-            const triangle = new Triangle(2,4,5);
-            expect(triangle.side1).toEqual(2);
-            expect(triangle.side2).toEqual(4);
-            expect(triangle.side3).toEqual(5);
-        });
+    test('should correctly creat a triangle obect with three lengths', () => {
+        const triangle = new Triangle(2, 4, 5);
+        expect(triangle.side1).toEqual(2);
+        expect(triangle.side2).toEqual(4);
+        expect(triangle.side3).toEqual(5);
+    });
+
+    test('should correctly determine whether three three lengths are not a triangle', () => {
+        const notTriangle = new Triangle(3, 9, 22);
+        expect(notTriangle.checkType()).toEqual("not a triangle");
+    });
 });
